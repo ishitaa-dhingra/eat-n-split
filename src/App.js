d5be2c40455c0be57d8054c62021667664e5f699
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2023-08-05 23:47:06 +0530

    building list of friends

--- src/App.js
+++ src/App.js
@@ -19,5 +19,44 @@ const initialFriends = [
   },
 ];
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <Friendlist />
+      </div>
+    </div>
+  );
+}
+
+function Friendlist() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name}></img>
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name}
+          {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name}are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
 }
