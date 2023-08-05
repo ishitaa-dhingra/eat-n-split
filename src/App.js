b5ecd0511dae11bef6c9d9e68551552ff1034697
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2023-08-06 02:03:05 +0530

    building static part

--- src/App.js
+++ src/App.js
@@ -23,7 +23,11 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <Friendlist />
+
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -56,7 +60,43 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name}are even</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑Friend Name</label>
+      <input type="text" />
+      <label>📸Image URL </label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form classNme="form-split-bill">
+      <h2>Split A Bill With X</h2>
+      <label>💰Bill Value</label>
+      <input type="text" />
+      <label>🕴️Your expense</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍👩X's expense</label>
+      <input type="text" />
+
+      <label>🤑Who is playing the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+    </form>
   );
 }
