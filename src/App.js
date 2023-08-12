a852255a54410a6c311ac0be443fbe3ac53ea343
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2023-08-12 23:45:39 +0530

    added key prop to render the error

--- src/App.js
+++ src/App.js
@@ -79,6 +79,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onsplit={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
